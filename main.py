2b595b461a7f0b898a4db2dc11f42548f024fc78
Arie Twigt <twigt.arie@gmail.com>
2023-12-12 09:53:30 +0100

    added additional argparse methods

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from custom_modules.calculation_functions import calc_circle
 import argparse
 
+float()
+int()
+str()
+[]
 
 # initate a argument parser instance
 parser = argparse.ArgumentParser()
@@ -17,6 +21,17 @@ parser.add_argument("--rounding", "-r",
                     default=1, 
                     help="Optional: Rounding for the value")
 
+parser.add_argument("--export_type", "-e",
+                    type=str,
+                    required=False,
+                    choices=['csv', 'print'],
+                    default='print')
+
+
+parser.add_argument("--diameters_list", "-dl",
+                    required=False,
+                    type=lambda s: [int(item) for item in s.split(',')])
+
 
 # parse the arguments
 args = parser.parse_args()
@@ -24,7 +39,14 @@ args = parser.parse_args()
 if __name__ == '__main__':
     my_diameter = args.diameter
     my_rounding = args.rounding
+    export_type = args.export_type
+    my_diameters_list = args.diameters_list
     
     my_circle = calc_circle(my_diameter, rounding=my_rounding)
     
-    print(my_circle)
+    # output based on the export type
+    if export_type == 'csv':
+        print("Exporting to csv")
+    else:
+        print(my_diameters_list)
+        print(my_circle)
