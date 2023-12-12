b1b6573f0a4627e289ec16291b70c18660288c4d
Arie Twigt <twigt.arie@gmail.com>
2023-12-12 14:08:31 +0100

    added example tests with text files

--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
 import pytest
+import re
+
 
 def calc_size(length, width):
 
@@ -16,6 +18,18 @@ def calc_size(length, width):
     return size
 
 
+def remove_vowels_text(text):
+
+    text_clean = re.sub(r'[AEOUI]', '', text, flags=re.IGNORECASE)
+
+    #export the file
+    with open("text_data/export/export_text.txt", "w") as file:
+        file.write(text_clean)
+
+    return text_clean
+
+
+
 # execute the tests
 def test_calc_size():
     assert calc_size(4, 4) == 16
@@ -25,3 +39,22 @@ def test_calc_size():
 def test_not_negative():
     with pytest.raises(ValueError):
         calc_size(-4, 10)
+
+
+def test_content_file():
+    # raw file, to test
+    with open("text_data/test_text.txt", "r") as file:
+        test_text_data = file.read()
+
+    # clean file, to compare with
+    with open("text_data/test_text_clean.txt", "r") as file:
+        test_text_data_clean = file.read()
+
+    # use the function
+    remove_vowels_text(test_text_data)
+    
+    # read the exporte text file
+    with open("text_data/export/export_text.txt", "r") as file:
+        exported_text_file = file.read()
+    
+    assert exported_text_file == test_text_data_clean
